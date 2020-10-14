8d6c2af55bf5d7f877c5b4f1c59a395c18e91cc3
Pat <stumbaup@oregonstate.edu>
2020-10-14 11:52:39 -0700

    added .csv test files

---
+++ main.c
@@ -0,0 +1,10 @@
+//create using:
+// gcc --std=gnu99 -o movies main.c
+//then run using
+// ./movies
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
